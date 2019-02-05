340845dede4dc9e3c6813b7bf427446f79cf99b7
Maciej Buszka <maciej@buszka.eu>
2019-02-05 12:58:21 +0100

    Introduction - types, algeff example

--- algeff/test/05.rkt
+++ algeff/test/05.rkt
@@ -1,13 +1,19 @@
 #lang algeff
 
 letrec fromTo x =
-  λ y if ==(x, y) then nil() else cons(x, fromTo +(x, 1) y) end
-in let map = λ f
-  letrec aux xs = if nil?(xs) then nil() else cons(f hd(xs), aux tl(xs)) end
-  in aux
-in let exists = λ p λ xs
-    handle map (λ x if (lift Break (p x)) then Break true else x end) xs with
-    | Break x r -> true
-    | return x  -> false
-    end
-in exists (λ x ==(x, 3)) (fromTo 0 10)
+  λ y if ==(x, y) then nil()
+    else cons(x, fromTo +(x, 1) y)
+    end in 
+let map = λ f
+  letrec loop xs = 
+    if nil?(xs) then nil()
+    else cons(f hd(xs), aux tl(xs))
+    end in 
+  loop in
+let exists = λ p λ xs
+  let f = λ x if p x then Break true else x end in
+  handle map f xs with
+  | Break x r -> true
+  | return x  -> false
+  end in
+exists (λ x ==(x, 3)) (fromTo 0 10)
