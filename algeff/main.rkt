94b18e11f37774c59b18c7d35ab5f9d2fba2ce97
Maciej Buszka <maciej@buszka.eu>
2019-02-06 21:11:30 +0100

    better racket module

--- algeff/main.rkt
+++ algeff/main.rkt
@@ -4,40 +4,49 @@
          syntax/strip-context
          "calculus/abstract-machine.rkt"
          "calculus/eval.rkt"
-         "calculus/type.rkt"
+         (for-syntax "calculus/type.rkt")
+         (for-syntax racket/base)
          "lang/parser.rkt"
          "lang/tokenizer.rkt")
 
-; Converts program text into a simple module which contains a calculus expression
-(define (read-syntax path port)
-  (define tokens (tokenize path port))
-  (define tree (parse tokens path))
-  (strip-context
-   #`(module algeff-tokenizer-mod algeff
-       #,tree)))
-
-(module+ reader (provide read-syntax))
+(module+ reader
+  ; Converts program text into a simple module which contains a calculus expression
+  (define (read-syntax path port)
+    (define tokens (tokenize path port))
+    (define tree (parse tokens path))
+    (strip-context
+     #`(module algeff-tokenizer-mod algeff
+         #,tree)))
+  (provide read-syntax))
 
 ; Expands the calculus expression into call to run function
-(define-syntax-rule (-#%module-begin tree)
-  (#%module-begin
-   (run (quote tree))))
-
-; Infers the type of an expression, then reduces it using both reduction relation and abstract machine
-(define (run e)
-  (begin
-    (printf "desugared expression:\n")
-    (pretty-print e)
-    (define t (infer-type e))
-    (if t
-        (begin
-          (printf "\nhas type:\n")
-          (pretty-print t)
-          (printf "\nreduction result:\n")
-          (pretty-print (reduce e))
-          (printf "\nabstract machine result:\n")
-          (pretty-print (am-reduce e)))
-        (printf "does not type-check\n"))))
+;(define-macro (-#%module-begin STX)
+;  #`(#%module-begin
+;   (define #,(datum->syntax caller-stx 'foo) 1)
+;   5))
+(define-syntax (-#%module-begin stx)
+  (syntax-case stx ()
+    [(_ tree)
+     (let* ([e (syntax->datum #'tree)]
+            [t (infer-type e)])
+       (with-syntax
+           ([expression (datum->syntax stx 'expression)]
+            [type (datum->syntax stx 'type)]
+            [traces-reduction (datum->syntax stx 'traces-reduction)]
+            [traces-machine (datum->syntax stx 'traces-machine)])
+         (if t
+             #`(#%module-begin
+                (require redex)
+                (define expression (quote tree))
+                (define type (quote #,t))
+                (define (traces-reduction)
+                  (traces red expression))
+                (define (traces-machine)
+                  (traces abstract-machine (term (initial-conf tree))))
+                (provide expression type))
+             (raise "does-not-typecheck"))))]
+    ))
 
 (provide (rename-out [-#%module-begin #%module-begin])
-         #%app #%datum #%top #%top-interaction)
+         #%top #%app #%datum #%top-interaction
+         )
